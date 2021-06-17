61c7745879ae89fbea8684b153f2591e8e720f91
zhangyu19980511 <40260950+zhangyu19980511@users.noreply.github.com>
2021-06-17 22:33:04 +0800

    Update client.js

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -1,9 +1,18 @@
 'use strict'
+// devices
 var videoplay = document.querySelector('video#player');
 var audioSource = document.querySelector("select#audioSource");
 var audioOutput = document.querySelector("select#audioOutput");
 var videoSource = document.querySelector("select#videoSource");
+
+// filter
 var filtersSelect = document.querySelector('select#filter');
+
+// picture
+var snapshot = document.querySelector('button#snapshot');
+var picture = document.querySelector('canvas#picture');
+picture.width = 320;
+picture.height = 240;
 
 function start() {
 	if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -63,6 +72,10 @@ start();
 
 videoSource.onchange = start;
 
-filtersSelect.onchange = function(){
+filtersSelect.onchange = function () {
 	videoplay.className = filtersSelect.value;
+}
+
+snapshot.onclick = function () {
+	picture.getContext('2d').drawImage(videoplay, 0, 0, picture.width, picture.height);
 }
