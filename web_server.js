6e254622bd072d24717f7634e121dc09fa80a582
zhangyu19980511 <40260950+zhangyu19980511@users.noreply.github.com>
2021-06-19 22:35:27 +0800

    创建聊天室页面

--- web_server.js
+++ web_server.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 var express = require('express');
 var serveIndex = require('serve-index');
 
+var socketIo = require('socket.io');
+
 var app = express();
 app.use(serveIndex('./public'));
 app.use(express.static('./public'));
@@ -22,4 +24,24 @@ var options = {
 
 // https server
 var https_server = https.createServer(options, app);
+var io = socketIo.listen(https_server);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('join', (room) => {
+        socket.join(room);
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = Object.keys(myRoom.sockets).length;
+        // 给房间里除了自己的所有人发
+        socket.to(room).emit('joined', room, socket.id);
+    });
+
+    socket.on('leave', (room) => {
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = Object.keys(myRoom.sockets).length;
+
+        socket.leave(room);
+        // 给房间里除了自己的所有人发
+        socket.to(room).emit('leaved', room, socket.id);
+    });
+});
 https_server.listen(8001);
